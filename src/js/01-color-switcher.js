fe33846d9bb5d3b08159c238bf0e5654db42bdd9
Dmitriy <dmitriy.botsman@gmail.com>
2021-11-16 15:52:15 +0200

    save

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const body = document.querySelector("body");
 
+const button = document.querySelector("button");
+const btnStop = document.querySelector("button[data-stop]");
+
+ 
+
+// function getRandomHexColor() {
+//    return`#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+
+// btnStart.addEventListener("click", rrr) 
+if (btnStart.dataset.start === "try") {
+     body.style.backgroundColor = "tomato";
+}
+    
+
+   
