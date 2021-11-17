bbe9c1ac8a4fb988d666ef44aa25d85b7f639f0a
Dmitriy <dmitriy.botsman@gmail.com>
2021-11-17 18:04:34 +0200

    dz3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+const formRef = document.querySelector('.form');
+const delayRef = document.querySelector('input[name="delay"]');
+const stepRef = document.querySelector('input[name="step"]');
+const amountRef = document.querySelector('input[name="amount"]');
+const butSab = document.querySelector('button');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  let delay = Number(delayRef.value);
+
+  for (let i = 0; i < Number(amountRef.value); i += 1) {
+    console.log(amountRef.value);
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(stepRef.value);
+  }
+});
